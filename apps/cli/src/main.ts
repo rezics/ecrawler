8ab0a70eaab3e9da0213e9489dc81337d0a4cce1
NMNM.CC <huangyinhaow@outlook.com>
2026-01-16 17:41:50 +0800

    refactor(cli): integrate NodeHttpClient layer for enhanced HTTP capabilities
    - Updated main.ts to include NodeHttpClient layer alongside NodeContext for improved HTTP client functionality.
    - Refactored program execution to utilize merged layers, enhancing modularity and maintainability of the CLI application.

--- apps/cli/src/main.ts
+++ apps/cli/src/main.ts
@@ -2,12 +2,12 @@ import {Command} from "@effect/cli"
 import pkg from "../package.json" with {type: "json"}
 import {Import} from "./commands/import.ts"
 import {Export} from "./commands/export.ts"
-import {NodeContext, NodeRuntime} from "@effect/platform-node"
-import {Effect} from "effect"
+import {NodeContext, NodeHttpClient, NodeRuntime} from "@effect/platform-node"
+import {Effect, Layer} from "effect"
 
 const program = Command.run(Command.make("ecrawler").pipe(Command.withSubcommands([Import, Export])), {
 	name: pkg.name,
 	version: pkg.version
 })
 
-program(process.argv).pipe(Effect.provide(NodeContext.layer), NodeRuntime.runMain)
+program(process.argv).pipe(Effect.provide(Layer.mergeAll(NodeContext.layer, NodeHttpClient.layer)), NodeRuntime.runMain)
