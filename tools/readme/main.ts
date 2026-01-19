194f1a79052d2a0b4745024c7db990bd581e1de6
NMNM.CC <huangyinhaow@outlook.com>
2026-01-19 19:12:13 +0800

    chore(e2e): introduce new E2E testing tools and update project structure
    - Removed the obsolete .dockerignore file to streamline the project.
    - Updated package.json to replace the e2e workspace with new tools for E2E testing.
    - Added new scripts for E2E testing and README generation in the tools directory.
    - Created new E2E testing files and configurations for improved testing capabilities.
    - Updated README generation logic to reflect the current project structure and dependencies.

--- tools/readme/main.ts
+++ tools/readme/main.ts
@@ -1,8 +1,8 @@
-import {$, cd} from "zx"
+import {cd} from "zx"
 import pkg from "../../package.json" with {type: "json"}
 import fs from "node:fs/promises"
 
-const root = $.env["PROJECT_CWD"]
+const root = process.env["PROJECT_CWD"]
 if (!root) {
 	throw new Error("PROJECT_CWD is not set")
 }
@@ -12,13 +12,6 @@ await fs.rm("README", {force: true})
 
 const readme = []
 
-readme.push(
-	`Effective`,
-	`Crawler @ ${pkg.version}`,
-	"",
-	await $`tree --gitignore --dirsfirst --prune --du -h`,
-	"",
-	`🄯 ${new Date().getFullYear()} NMNM.CC, REZICS`
-)
+readme.push(`Effective`, `Crawler @ ${pkg.version}`, "", `🄯 ${new Date().getFullYear()} NMNM.CC, REZICS`)
 
 await fs.writeFile("README", readme.join("\n"), "utf-8")
